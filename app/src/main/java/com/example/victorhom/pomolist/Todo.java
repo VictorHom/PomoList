4477f83b85b657634bfdf606e2522c1fa6bb49f5
Victor Hom <victorhom@Victors-MBP-2.home>
2016-09-13 23:26:27 -0400

    clean up the todo class

--- app/src/main/java/com/example/victorhom/pomolist/Todo.java
+++ app/src/main/java/com/example/victorhom/pomolist/Todo.java
@@ -2,7 +2,7 @@ package com.example.victorhom.pomolist;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
+import java.util.Calendar;
 import java.util.List;
 
 /**
@@ -11,24 +11,27 @@ import java.util.List;
 public class Todo {
     private String todo; // the to-do
     private String note; // note about to-do
-    private Date date; // date created
-    private Date edited;
     private boolean pomo; // if in pomo list
+    private int day;
+    private int month;
+    private int year;
 
     // for testing
     // goal - add to SQL and have a persistent backend
     public static List<Todo> myList = new ArrayList<>(Arrays.asList(
-            new Todo("Buy Grocery", "Tomato, Notepad"),
-            new Todo("Take Shower", "It\'s been a few days"),
-            new Todo("Work on Todo List", "Submit project by 9/23")
+            new Todo("Buy Grocery"),
+            new Todo("Take Shower"),
+            new Todo("Work on Todo List")
             )
     );
 
-
-
-    Todo(String todo, String note) {
+    Todo(String todo) {
         this.todo = todo;
-        this.note = note;
+        this.note = "";
+        this.pomo = false;
+        this.day = Calendar.DAY_OF_MONTH;
+        this.month = Calendar.MONTH;
+        this.year = Calendar.YEAR;
     }
 
     public String getTodo() {
@@ -38,6 +41,31 @@ public class Todo {
     public String setTodo(String updatedTodo) {
         this.todo = updatedTodo;
         return this.todo;
+    }
+
+    public String setNote(String note) {
+        this.note = note;
+        return note;
+    }
+
+    public boolean togglePomo() {
+        this.pomo = !this.pomo;
+        return this.pomo;
+    }
+
+    public int setDay(int day) {
+        this.day = day;
+        return day;
+    }
+
+    public int setMonth(int month) {
+        this.month = month;
+        return month;
+    }
+
+    public int setYear(int year) {
+        this.year = year;
+        return year;
     }
 
     public String getDescription() {
